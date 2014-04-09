16ce0ba11dc3d19c4a2feca1a395b4e185176ed3
asimkhaja <asim.khaja@gmial.com>
2014-04-09 16:27:49 -0700

    Integrated creatation of user realm to obs call

--- SparkLessons/src/main/java/com/intuit/tutor/CustomerController.java
+++ SparkLessons/src/main/java/com/intuit/tutor/CustomerController.java
@@ -1,21 +1,11 @@
 package com.intuit.tutor;
 
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 import java.util.UUID;
 
-import javax.ws.rs.NotSupportedException;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.xml.bind.JAXBElement;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.cxf.jaxrs.impl.MetadataMap;
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -23,6 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.intuit.platform.integration.consumer.JSONMarshaller;
+import com.intuit.platform.integration.iface.IMarshaller;
+import com.intuit.platform.integration.iface.IntuitHeader;
+import com.intuit.platform.integration.ius.common.types.ChallengeQuestionAnswer;
+import com.intuit.platform.integration.ius.common.types.Email;
+import com.intuit.platform.integration.ius.common.types.FullName;
+import com.intuit.platform.integration.ius.common.types.IAMTicket;
+import com.intuit.platform.integration.ius.common.types.ObjectFactory;
+import com.intuit.platform.integration.ius.common.types.User;
 import com.intuit.tutor.entity.Address;
 import com.intuit.tutor.entity.BankAccount;
 import com.intuit.tutor.entity.Customer;
@@ -30,26 +29,6 @@ import com.intuit.tutor.obs.MerchantApplicationRequest;
 import com.intuit.tutor.obs.MerchantApplicationResponse;
 import com.intuit.tutor.obs.OnboardingServiceClient;
 import com.intuit.tutor.security.Token;
-import com.intuit.platform.integration.consumer.GSONMarshaller;
-import com.intuit.platform.integration.consumer.JAXBXMLMarshaller;
-import com.intuit.platform.integration.consumer.JSONMarshaller;
-import com.intuit.platform.integration.httpclient.ApacheHttpClient;
-import com.intuit.platform.integration.httpclient.OIIRequestHeaderHandler;
-import com.intuit.platform.integration.iface.IHttpClient;
-import com.intuit.platform.integration.iface.IMarshaller;
-import com.intuit.platform.integration.iface.ISecurity;
-import com.intuit.platform.integration.iface.IntuitHeader;
-import com.intuit.platform.integration.iface.ISecurity.APIAuthMethod;
-import com.intuit.platform.integration.ius.common.types.AcceptInvitationRequest;
-import com.intuit.platform.integration.ius.common.types.ChallengeQuestionAnswer;
-import com.intuit.platform.integration.ius.common.types.Email;
-import com.intuit.platform.integration.ius.common.types.FullName;
-import com.intuit.platform.integration.ius.common.types.IAMTicket;
-import com.intuit.platform.integration.ius.common.types.IAMTicketSignInRequest;
-import com.intuit.platform.integration.ius.common.types.ObjectFactory;
-import com.intuit.platform.integration.ius.common.types.User;
-import com.intuit.platform.integration.rest.client.IUSRestClient;
-import com.intuit.psd.cdm.v1.*;
 
 @Controller
 public class CustomerController {
@@ -117,20 +96,28 @@ public class CustomerController {
 				+ address.getStreetAddress1() + " " + address.getCity() + " "
 				+ address.getState() + " " + address.getZipCode());
 
+		customer.setPhone("8188188818");
+		customer.setPassword("testiususer");
+		BankAccount bankAccount = new BankAccount();
+		bankAccount.setAccountNumber("9876543210");
+		bankAccount.setRoutingNumber("122000661");
+		bankAccount.setBankName("Bank of America");
 		
-//		createIAMUser(customer, address);
+		String guuid = UUID.randomUUID().toString().replace("-", "");
+		//customer.setEmail("tester60af3c9852b94db6a6cc1e4b7f73e3e1@test.intuit.com");
+		customer.setEmail("tester" + guuid + "@test.intuit.com");
+
+		
+		IAMTicket iamticket = createIAMUser(customer, address);
 
 		// Send create account request
 		// String str = iam.signup(id, "127.0.0.1", getAuthHeader(),
 		// getOfferingId());
 		// System.out.println(str);
 //		IAMToken iamToken = new IAMToken();
-		String ticket = "V1-203-Q3f10vwsjk8pqkuqfwcgeb";//iamToken.getTicket();
-		String authId = "1056553675";//iamToken.getUserId();
-		customer.setEmail("tester60af3c9852b94db6a6cc1e4b7f73e3e1@test.intuit.com");
-		
-		BankAccount bankAccount = new BankAccount();
-		
+		String ticket = iamticket.getTicket();//iamToken.getTicket();
+		String authId = iamticket.getUserId();//iamToken.getUserId();
+
 		Token token = new Token();
 		token.setAuthId(authId);
 		token.setTokenId(ticket);
@@ -145,63 +132,53 @@ public class CustomerController {
 		return "order";
 	}
 
-	private User createIAMUser(Customer customer, Address customerAddress)
+	private IAMTicket createIAMUser(Customer customer, Address customerAddress)
 			throws Exception {
 		
 		User user = new User();
 		String guuid = UUID.randomUUID().toString().replace("-", "");
-		user.setUsername("iustestuser" + guuid);
-		user.setPassword("testiususer");
+		user.setUsername(customer.getEmail());
+		user.setPassword(customer.getPassword());
 		user.setSecurityLevel("HIGH");
 		ChallengeQuestionAnswer qa = new ChallengeQuestionAnswer();
 		qa.setQuestion("Who is your all-time favorite movie character?");
 		qa.setAnswer("Elmo");
 		user.getChallengeQuestionAnswer().add(qa);
 		Email email = new Email();
-		email.setAddress("tester" + guuid + "@test.intuit.com");
+		email.setAddress(customer.getEmail());
 		email.setPrimary(Boolean.TRUE);
 		user.setEmail(email);
 		com.intuit.platform.integration.ius.common.types.Address address = new com.intuit.platform.integration.ius.common.types.Address();
 		address.setType("MAIN");
-		address.setAddress1("12345 Wonder Lane");
-		address.setCityOrLocality("Hollywood");
-		address.setPostalCode("90210");
-		address.setStateOrProvince("CA");
+		address.setAddress1(customerAddress.getStreetAddress1());
+		address.setCityOrLocality(customerAddress.getCity());
+		address.setPostalCode(customerAddress.getZipCode());
+		address.setStateOrProvince(customerAddress.getState());
 		address.setCountry("US");
 		user.getAddress().add(address);
 		FullName fullName = new FullName();
-		fullName.setGivenName("O'neil");
-		fullName.setMiddleName("O'reilly");
-		fullName.setSalutation("Jr'");
-		fullName.setSuffix("O'");
-		fullName.setSurName("O'neil");
+		fullName.setGivenName(customer.getFirstname());
+		//fullName.setMiddleName();
+		//fullName.setSalutation("Jr'");
+		//fullName.setSuffix("O'");
+		fullName.setSurName(customer.getLastname());
 		user.getFullName().add(fullName);
-		//iusClient.signup(user);
 		
 		JAXBElement<User> createdUserElement = iamRESTClient.signup(guuid, objectFactory.createUser(user));
 		User newuser = createdUserElement.getValue();
+		newuser.setPassword(user.getPassword());
 		// validate user is created
 		
 		String body = "{\"userId\":\"" + newuser.getUserId() + "\",\"password\":\"" + user.getPassword() + "\"}";
 		System.out.println("body: " + body);
-		try {
-			JAXBElement<IAMTicket> iamTicketElement = iamRESTClient.signin(guuid, body);
-			IAMTicket ticket = iamTicketElement.getValue();
-			System.out.println("UserId :" + ticket.getUserId());
-			System.out.println("Ticket :" + ticket.getTicket());
-			System.out.println("RealmId :" + ticket.getRealmId());
-		} catch (NotSupportedException e) {
-			System.out.println(e.getResponse().getEntity());
-		}
-	
 		
-//
-//		User retUser = iusClient.getUserByUserId(ticket.getTicket(),
-//				ticket.getUserId());
-//		System.out.println("  Email :" + retUser.getEmail().getAddress());
-
-		//return retUser;
-		return user;
+		JAXBElement<IAMTicket> iamTicketElement = iamRESTClient.signin(guuid, body);
+		IAMTicket ticket = iamTicketElement.getValue();
+		System.out.println("UserId :" + ticket.getUserId());
+		System.out.println("Ticket :" + ticket.getTicket());
+		System.out.println("RealmId :" + ticket.getRealmId());
+		
+		return ticket;
 	}
 
 
