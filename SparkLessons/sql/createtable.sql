fcc72478185cee494445a182b7918863c3b3a19c
powerdbyintellect <harish_coundinya@yahoo.com>
2014-04-09 21:32:48 -0700

    checking in sql file

---
+++ SparkLessons/sql/createtable.sql
@@ -0,0 +1,12 @@
+Drop table users;
+
+CREATE TABLE users (
+userid varchar (20),
+password varchar(20),
+email varchar(100),
+firstname varchar (50),
+lastname varchar (50),  
+masteraccount varchar (10),
+realmid varchar (20),
+accountnumber varchar(20),
+loginid varchar(50));
