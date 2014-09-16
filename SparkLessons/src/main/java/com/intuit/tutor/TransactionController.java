9b40d78e0f667d43f9ca7f6d4452df232d824cea
asimkhaja <asim.khaja@gmial.com>
2014-09-16 16:25:02 -0700

    Added Twitter Posts Back
    Added Twitter Posts Back

--- SparkLessons/src/main/java/com/intuit/tutor/TransactionController.java
+++ SparkLessons/src/main/java/com/intuit/tutor/TransactionController.java
@@ -1,28 +1,17 @@
 package com.intuit.tutor;
 
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.io.StringWriter;
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.namespace.QName;
-import javax.xml.transform.Result;
-import javax.xml.transform.stream.StreamResult;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.oxm.XmlMappingException;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,9 +21,6 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.auth.AccessToken;
-import twitter4j.auth.RequestToken;
-import twitter4j.conf.ConfigurationBuilder;
 
 import com.intuit.payments.sdk.jaxb.types.CreditCard;
 import com.intuit.payments.sdk.jaxb.types.CreditCardCharge;
@@ -46,6 +32,8 @@ import facebook4j.FacebookException;
 
 @Controller
 public class TransactionController {
+	
+	private static Logger log = Logger.getLogger(TransactionController.class);
 	
 	@Autowired
 	private PaymentsRESTInterface restClient;
@@ -101,28 +89,28 @@ public class TransactionController {
 		//Twitter Integration 
 //		https://apps.twitter.com/app/6866796/keys
 		// The factory instance is re-useable and thread safe.
-//	    TwitterFactory factory = new TwitterFactory();
-//	    AccessToken accessToken = null;
-//	    Twitter twitter = factory.getInstance();
-//	    twitter.setOAuthConsumer("CdU4znJoWKeylArzs5dHsZPhb", "ZhNAuzC4cYxPkkYwlj9A32cm35E2v8a7SolkzGUk73hysDBqZv");
-//	    twitter.setOAuthAccessToken(accessToken);
-//	    
-//		 // The factory instance is re-useable and thread safe.
-//	    Status status;
-//		try {
-//			RequestToken requestToken = twitter.getOAuthRequestToken();
-//            System.out.println(requestToken.getAuthorizationURL());
-//            accessToken = twitter.getOAuthAccessToken(requestToken);
-//    		status = twitter.updateStatus("I just got paid $"+charge.getAmount()+" for giving "+lessonType+" Lessons !!! ");
-//    		System.out.println("Got access token.");
-//            System.out.println("Access token: " + accessToken.getToken());
-//            System.out.println("Access token secret: " + accessToken.getTokenSecret());
-//			System.out.println("Successfully updated the status to [" + status.getText() + "].");
-//		} catch (TwitterException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	    
+	    /*TwitterFactory factory = new TwitterFactory();
+	    AccessToken accessToken = null;
+	    Twitter twitter = factory.getInstance();
+	    twitter.setOAuthConsumer("CdU4znJoWKeylArzs5dHsZPhb", "ZhNAuzC4cYxPkkYwlj9A32cm35E2v8a7SolkzGUk73hysDBqZv");
+	    twitter.setOAuthAccessToken(accessToken);*/
+		Twitter twitter = TwitterFactory.getSingleton();
+	     // The factory instance is re-useable and thread safe.
+	    Status status;
+		try {
+			//RequestToken requestToken = twitter.getOAuthRequestToken();
+            //log.debug(requestToken.getAuthorizationURL());
+            //accessToken = twitter.getOAuthAccessToken(requestToken);
+    		status = twitter.updateStatus("I just got paid $"+charge.getAmount()+" for giving "+lessonType+" Lessons !!! ");
+    		log.debug("Got access token.");
+            //log.debug("Access token: " + accessToken.getToken());
+            //log.debug("Access token secret: " + accessToken.getTokenSecret());
+			log.debug("Successfully updated the status to [" + status.getText() + "].");
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			log.error(e);
+		}
+	    
 		return mav; //new ModelAndView("charge", "creditCardResponse", response.getValue());
         //return new ModelMap(response.getValue());
 		//return "charge";
